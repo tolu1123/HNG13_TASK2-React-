c12e4b50b75be9ede5cb5d263f7c0057c5b2286b
Ojo Tolulope <toluene1123@gmail.com>
2025-10-29 12:50:00 +0100

    Added max screen size

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,13 @@
-import { LoginForm } from "@/components/auth/login-form"
+import { LoginForm } from "@/components/auth/login-form";
 
 export default function LoginPage() {
   return (
-    <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
-      <div className="w-full max-w-sm md:max-w-4xl">
-        <LoginForm />
+    <div className='w-full'>
+      <div className='max-w-[1440px] mx-auto bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10'>
+        <div className='w-full max-w-sm md:max-w-4xl'>
+          <LoginForm />
+        </div>
       </div>
     </div>
-  )
+  );
 }
